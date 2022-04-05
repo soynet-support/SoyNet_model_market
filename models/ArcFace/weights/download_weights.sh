642abdf8ed956b429f6b23baa077a9b871892697
inwoo <support@soynet.io>
2022-04-06 04:31:43 +0900

    arcface downlaod_weights

---
+++ models/ArcFace/weights/download_weights.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+file_id="1zmrXlsZnpnmFGZfWtSi9jIgw3dUUcnKE"
+file_name="arc_face_r18.weights"
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${file_id}" > /dev/null
+code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${file_id}" -o ${file_name}
