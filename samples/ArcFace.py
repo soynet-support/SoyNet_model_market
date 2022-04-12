e4a69e8fcd461e8c51a59eaa8fa092f6079c2b47
soynet-support <74886743+soynet-support@users.noreply.github.com>
2022-04-12 13:49:45 +0900

    Update ArcFace.py

--- samples/ArcFace.py
+++ samples/ArcFace.py
@@ -38,9 +38,6 @@ if __name__ == "__main__":
 
     # Read Test Data
     img = cv.imread("../data/baby_x2_bicubic.jpg")
-    if img is None:
-        print("Image is None!")
-        sys.exit()
 
     # Resize Image
     resized_img = cv.resize(img, (model_width, model_height))
