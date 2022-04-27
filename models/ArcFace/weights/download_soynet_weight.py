3c47ace24a90ce7b1d90f363ac244b75bacdb85c
inwoo <support@soynet.io>
2022-04-27 14:34:59 +0900

    Modify download_soynet_weight

--- models/ArcFace/weights/download_soynet_weight.py
+++ models/ArcFace/weights/download_soynet_weight.py
@@ -2,9 +2,9 @@ import wget
 
 if __name__ == '__main__':
     weight_url = [
-        'https: // kr.object.iwinv.kr / model_market_weight / arc_face_r100.weights',
-        'https: // kr.object.iwinv.kr / model_market_weight / arc_face_r18.weights',
-        'https: // kr.object.iwinv.kr / model_market_weight / arc_face_r50.weights'
+        'https://kr.object.iwinv.kr/model_market_weight/arc_face_r100.weights',
+        'https://kr.object.iwinv.kr/model_market_weight/arc_face_r18.weights',
+        'https://kr.object.iwinv.kr/model_market_weight/arc_face_r50.weights'
     ]
 
     for url in weight_url:
