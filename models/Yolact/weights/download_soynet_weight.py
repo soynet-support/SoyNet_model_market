ba33050ee305e7a514e18fa439a8d656b22256ae
soynet-support <74886743+soynet-support@users.noreply.github.com>
2022-06-27 15:18:00 +0900

    remove downloading link of resnet50 weight file

--- models/Yolact/weights/download_soynet_weight.py
+++ models/Yolact/weights/download_soynet_weight.py
@@ -3,7 +3,6 @@ import wget
 if __name__ == '__main__':
     weight_url = [
         'https://kr.object.iwinv.kr/model_market_weight/yolact-resnet101.weights',
-        'https://kr.object.iwinv.kr/model_market_weight/yolact-resnet50.weights'
     ]
 
     for url in weight_url:
